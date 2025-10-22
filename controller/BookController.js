84d73182d62af90d0ee97e1980b3c62ed375e973
jsyoon27 <tjddbswkd1@naver.com>
2025-10-22 23:52:45 +0900

    1022

--- controller/BookController.js
+++ controller/BookController.js
@@ -11,7 +11,7 @@ const allBooks = (req, res) => {
     // limit * (currentPage - 1)
     let offset = limit * (currentPage - 1);
 
-    let sql = "SELECT * FROM books";
+    let sql = "SELECT *, (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes FROM books";
     let values = [];
 
     if (category_id && news) {
@@ -43,11 +43,18 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-    let {id} = req.params;
+    let {user_id} = req.body;
+    let book_id = req.params.id;
     
-    let sql = `SELECT * FROM books LEFT
-                JOIN category ON books.category_id = category.id where books.id = ?;`;
-    conn.query(sql,id,
+    let sql = `SELECT *,
+        (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes,
+        (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked
+        FROM books 
+        LEFT JOIN category
+        ON books.category_id = category.category_id 
+        WHERE books.id=?;`;
+    let values = [user_id, book_id, book_id];
+    conn.query(sql,values,
         (err, results) => {
             if (err) {
                 console.error(err);
