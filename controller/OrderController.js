4aa3de023a39a2d080f1c1ae1d639902a899b163
jsyoon27 <tjddbswkd1@naver.com>
2025-10-29 00:43:01 +0900

    1028

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -29,7 +29,7 @@ const order = async (req, res) => {
 
     // items를 가지고, 장바구니에서 book_id, quantity 조회
     sql = 'SELECT book_id,quantity FROM cartItems WHERE id IN (?)';
-    let [ orderItems, fields] = await conn.query(sql,[items]);
+    let [orderItems, fields] = await conn.query(sql,[items]);
     // orderedBook 테이블 삽입
     sql =`INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?;`
     // items.. 배열 : 요소들을 하나씩 꺼내서 (foreach문 돌려서)
@@ -52,11 +52,40 @@ const deleteCartItems = async (conn,items) => {
     return result;
 }
 
-const getOrders = (req, res) => {
-    res.send('주문 목록 조회');
-}
-const getOrderDetail = (req, res) => {
-    res.send('주문 상세 상품 조회');
+const getOrders = async (req, res) => {
+    const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        port: 3307,
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        dateStrings: true
+    });
+    let sql = `SELECT orders.id, created_at, address, receiver, contact,
+            book_title, total_quantity,total_price
+            FROM orders LEFT JOIN delivery
+            ON orders.delivery_id = delivery.id;`
+    let [rows,fields] = await conn.query(sql);
+    return res.status(StatusCodes.OK).json(rows);
+};
+
+const getOrderDetail = async (req, res) => {
+    const {id} = req.params;
+
+        const conn = await mariadb.createConnection({
+        host: '127.0.0.1',
+        port: 3307,
+        user: 'root',
+        password: 'root',
+        database: 'Bookshop',
+        dateStrings: true
+    });
+    let sql = `SELECT book_id, title, author, price, quantity 
+                FROM orderedBook LEFT JOIN books
+                ON orderedBook.book_id = books.id
+                WHERE order_id=?`;
+    let [rows,fields] = await conn.query(sql,[id]);
+    return res.status(StatusCodes.OK).json(rows);
 }
 
 module.exports = {
